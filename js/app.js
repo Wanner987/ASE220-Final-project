2fca5d15323fff382d6446e89c62bc6a02b07f79
Tanner <tannerandrews859@gmail.com>
2025-05-01 10:37:27 -0400

    added conneciton to db

--- js/app.js
+++ js/app.js
@@ -8,25 +8,43 @@ const { MongoClient, ObjectId, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://whatever:whatever123123123@cluster0.c0e2tij.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-app.post('/api', (req, res)=> {
+var db;
+//var database = "sample_mflix";
+
+//connect to db and listen to port
+start();
+console.log('dffff')
+app.post('/api', async function(req, res) {
 	console.log('posted')
     res.send('post request');
-})
-app.get('/api', (req, res)=> {
-	console.log('get')
+});
+app.get('/api', async function(req, res) {
+    console.log('get');
     res.send('get request');
-})
-app.put('/api', (req, res)=> {
+});
+app.put('/api', async function(req, res) {
 	console.log('put')
     res.send('put request');
-})
-app.delete('/api', (req, res)=> {
+});
+app.delete('/api', async function(req, res) {
 	console.log('deleted')
     res.send('delete request');
-})
+});
 
+async function findCollection(db, database, collection) {
+    let result = await db.db(database).collection(collection).find({});
+    return result;
+}
 
-//Listen to port
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+async function connectToMongo() {
+    let connection = await client.connect();
+    return connection;
+}
+
+async function start() {
+    db = await connectToMongo();
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+      });
+}
